cb827e79dd2792ad02ddc2633029a771fa4176ce
Inigo Dela Vega <143131551+KaguraInig0@users.noreply.github.com>
2023-08-28 16:31:58 -0700

    changed code
    dd

--- FishingGame/Assets/InteractableObject.cs
+++ FishingGame/Assets/InteractableObject.cs
@@ -1,12 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InteractableObject : MonoBehaviour
 {
+    public int maxDistance = 3;
+    public int currentDistance = 0;
+    public Vector3 mousePos;
+    public Vector3 worldPos;
+    /*
+     We need the garbage object to follow at a offset with a velocity
+    We will accomplish this by: 
+    give player a distance from garbage variable
+    give player a current distance from garbage variable
+    give garbage a speed variable 
+    let player check distance from mouse to distance from garbage
+    let player check garbage speed 
+    if garbage distance from player >= max and garbage speed = maxSpeed
+        pointMouseDirection
+        cut the rope
+    if garbage distance from player < maxDistance and garbage speed <= maxSpeed
+        pointMouseDirection
+    if garbage distance from player == maxDistance and garbage speed < maxSpeed
+        pointMouseDirection
+        increases speed to mouseSpeed
+    if garbageDistancetoPlayer < maxDistance and ObjSpeed = maxSpeed
+        point to mouseDireciton
+        friction = mu * garbageWeight
+        
+        
+        
     
-    public  Vector3 mousePos;
-    public  Vector3 worldPos;
+    
+    
+     */
 
     private bool isMouseOver = false;
     
@@ -35,6 +63,11 @@ public class InteractableObject : MonoBehaviour
                 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
 
                 transform.position = worldPos;
+
+                if (maxDistance < 3)
+                {
+
+                }
             }
 
             else 
